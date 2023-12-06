72144656d3b4fedb324aa9a0c38afbe7fa6345da
highlander <highlander9200@gmail.com>
2023-12-06 07:48:42 -0300

    Initial commit

---
+++ src/main/java/com/highlander/DesafioInjecaoDeDependencia/DesafioInjecaoDeDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.highlander.DesafioInjecaoDeDependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInjecaoDeDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInjecaoDeDependenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highlander/DesafioInjecaoDeDependencia/DesafioInjecaoDeDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highlander.DesafioInjecaoDeDependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioInjecaoDeDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
